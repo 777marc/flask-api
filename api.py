8bbcc5257e1c505577a554343c7f665ca4ae0c8b
Marc Mendoza <777marc@gmail.com>
2025-06-12 11:44:41 -0400

    add routes

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+api = Api(app)
 
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,9 +15,36 @@ class UserModel(db.Model):
     def __repr__(self):
         return f"User(name = {self.name}, email = {self.email})"
 
+user_args = reqparse.RequestParser()
+user_args.add_argument('name', type=str, required=True, help="Name can not be blank")
+user_args.add_argument('email', type=str, required=True, help="Email can not be blank")
+
+userFields = {
+    'id':fields.Integer,
+    'name':fields.String,
+    'email':fields.String,
+}
+
+class Users(Resource):
+    @marshal_with(userFields)
+    def get(self):
+        users = UserModel.query.all()
+        return users
+    
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args["name"], email=args["email"])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 201
+
+api.add_resource(Users, '/api/users/')
+
 @app.route('/')
 def home():
-    return '<h1>flask rest api<h1/>'
+    return '<h1>flask rest api</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
